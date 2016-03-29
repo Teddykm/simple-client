be1123f0b7f92f37606b7a15aa96ef99c4368d4b
teddy.kilonzo <teddy.kilonzo@ft.com>
2016-03-29 15:38:08 +0100

    Created a client class to abstract cassandra requests and an application class

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,14 @@
+import com.example.cassandra.SimpleClient;
+
+
+public class SimpleApp {
+
+    public static void main (String[] args) {
+        SimpleClient client = new SimpleClient();
+        client.connect("127.0.0.1");
+        //client.createSchema();
+        //client.loadData();
+        client.querySchema();
+        client.close();
+    }
+}
